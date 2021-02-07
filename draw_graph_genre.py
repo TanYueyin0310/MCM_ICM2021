9cf303b9f003db6c261a003e6cd520797ebe5e7e
Tan Yueyin <tanyueyin@foxmail.com>
2021-02-07 19:29:50 +0800

    to draw influencer2follower network by genre

--- draw_graph_genre.py
+++ draw_graph_genre.py
@@ -1,19 +1,33 @@
 import networkx as nx
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 G = nx.DiGraph()
 nodelist = []
 filename = 'influence_data_by_genre_weight.csv'
-with open(filename, encoding='gb18030', errors='ignore') as csvfile:
+with open(filename, errors='ignore') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        influencer_genre = row['influencer_main_genre_flag']
-        follower_genre = row['follower_main_genre_flag']
+        influencer_genre = row['influencer_genre_id']
+        follower_genre = row['follower_genre_id']
         if not (influencer_genre in nodelist):
-            G.add_node(influencer_genre)
+            G.add_node(influencer_genre,id=row['influencer_genre_id'])
             nodelist.append(influencer_genre)
         if not (follower_genre in nodelist):
-            G.add_node(follower_genre)
+            G.add_node(follower_genre,id = row['follower_genre_id'])
             nodelist.append(follower_genre)
         G.add_edge(influencer_genre, follower_genre, weight=row['weight'])
 
-a
+options = {
+    'arrowstyle': '->',
+}
+pos = nx.random_layout(G)
+plt.figure(figsize=(15, 9))
+nx.draw_networkx_nodes(G, pos,alpha=0.9, node_size=400)
+for i in G.nodes:
+    for j in G.nodes:
+        if j in G[i]:
+            nx.draw_networkx_edges(G, pos, G.edges([i, j]), width=4*(float(G.edges[i, j]['weight'])),**options)
+node_labels = nx.get_node_attributes(G,'id')
+nx.draw_networkx_labels(G, pos, font_size=10, labels = node_labels)
+plt.show()
